035a3d1a1f3ae3803bf64fc5e51bc5acfc8d5120
Matthew Macdonald-Wallace <matt@doics.co>
2022-04-27 12:28:04 +0100

    Initial dummy function

--- pyecb/__init__.py
+++ pyecb/__init__.py
@@ -1,1 +1,28 @@
+# -*- coding: utf-8 -*-
 __version__ = '0.1.0'
+
+import requests
+
+class ECB:
+
+    def __init__(
+            username=None,
+            api_key=None):
+        """
+        ECB
+
+        Create a new instance of the ECB API
+
+        Args:
+            username (str): The Username supplied for the API
+            api_key (str): The API Key for authentication
+
+        """
+        self.username = username
+        self.api_key = api_key
+
+        # login and get the session token
+        self.login()
+
+    def login(self):
+        pass
