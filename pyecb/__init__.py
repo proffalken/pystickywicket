60493ae21476440eeac7a17a3929db0e78a8c065
Matthew Macdonald-Wallace <matt@doics.co>
2022-04-27 12:32:39 +0100

    Add flake8 to poetry and remove unneeded import

--- pyecb/__init__.py
+++ pyecb/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 __version__ = '0.1.0'
 
-import requests
 
 class ECB:
 
     def __init__(
+            self,
             username=None,
             api_key=None):
         """
