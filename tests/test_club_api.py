abc31b7a6dd4837aee69101e3e4f0470291f4fdc
Matthew Macdonald-Wallace <matt@doics.co>
2022-09-14 07:07:48 +0100

    Removed more references to ECB

--- tests/test_club_api.py
+++ tests/test_club_api.py
@@ -3,6 +3,6 @@ import pystickywicket
 
 
 def test_get_club_Data():
-    stickywicket = pystickywicket.ECB(api_key="test")
+    stickywicket = pystickywicket.StickyWicket(api_key="test")
     cd = stickywicket.get_club_data(club_id="Monmouth")
     assert cd['status'] == 401
--- tests/test_login.py
+++ tests/test_login.py
@@ -3,16 +3,16 @@ import pystickywicket
 
 
 def test_class_loads():
-    stickywicket = pystickywicket.ECB(api_key="test")
+    stickywicket = pystickywicket.StickyWicket(api_key="test")
 
     assert stickywicket.api_key == "test"
 
 def test_get_content_failing_login():
-    stickywicket = pystickywicket.ECB(api_key="test")
+    stickywicket = pystickywicket.StickyWicket(api_key="test")
     gc = stickywicket._get_api_content(api_name="clubs", club_id="Monmouth")
     assert gc['status'] == 401
 
 def test_get_content_successful_login():
-    stickywicket = pystickywicket.ECB(api_key=os.getenv("ECB_API_KEY"))
+    stickywicket = pystickywicket.StickyWicket(api_key=os.getenv("StickyWicket_API_KEY"))
     gc = stickywicket._get_api_content(api_name="clubs", club_id="Monmouth")
     assert gc['status'] == 401
